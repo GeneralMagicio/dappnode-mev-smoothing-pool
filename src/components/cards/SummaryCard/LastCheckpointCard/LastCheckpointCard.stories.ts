e0cf77184683f479dcda8b47094b7a34fecb6eb7
Vitor <marthendalnunes@gmail.com>
2023-03-29 10:52:59 -0300

    chore: fix storybook mock of LastCheckpointCard

--- src/components/cards/SummaryCard/LastCheckpointCard/LastCheckpointCard.stories.ts
+++ src/components/cards/SummaryCard/LastCheckpointCard/LastCheckpointCard.stories.ts
@@ -9,4 +9,27 @@ const meta: Meta<typeof LastCheckpointCard> = {
 export default meta
 type Story = StoryObj<typeof LastCheckpointCard>
 
-export const Primary: Story = {}
+export const Primary: Story = {
+  args: {
+    lastCheckpointDate: new Date('2023-03-20T00:00:00.000Z'),
+    nextCheckpointDate: new Date('2023-04-30T00:00:00.000Z'),
+  },
+}
+
+export const Loading: Story = {
+  args: {
+    lastCheckpointDate: new Date('2023-03-20T00:00:00.000Z'),
+    nextCheckpointDate: new Date('2023-04-30T00:00:00.000Z'),
+    isError: false,
+    isLoading: true,
+  },
+}
+
+export const Error: Story = {
+  args: {
+    lastCheckpointDate: new Date('2023-03-20T00:00:00.000Z'),
+    nextCheckpointDate: new Date('2023-04-30T00:00:00.000Z'),
+    isError: true,
+    isLoading: false,
+  },
+}
