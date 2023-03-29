ef430a370dad16dfba2cf46d80b21b80ea8c6bab
Vitor <marthendalnunes@gmail.com>
2023-03-28 22:16:10 -0300

    feat: add footer

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,9 +3,12 @@ import { mainnet, goerli } from '@wagmi/chains'
 export const SITE_NAME = 'Next.js Web3 Starter'
 export const SITE_DESCRIPTION = 'Next.js + Web3 powered by General Magic.'
 export const SITE_URL = 'https://starter.generalmagic.io/'
+export const MAIN_SITE_URL = 'https://dappnode.com/'
 
-export const SOCIAL_TWITTER = 'generalmagicio'
-export const SOCIAL_GITHUB = 'GeneralMagicio/next-web3-starter'
+export const SOCIAL_DISCORD = 'dappnode'
+export const SOCIAL_TWITTER = 'dappnode'
+export const SOCIAL_GITHUB = 'dappnode'
+export const SOCIAL_LINKEDIN = 'dappnode'
 
 export const WEB3_CHAINS = [mainnet, goerli]
 
@@ -27,6 +30,25 @@ export const PAGES = [
     path: 'https://dappnode.com/',
   },
 ]
+
+export const SOCIALS = [
+  {
+    name: 'LinkedIn',
+    path: `https://www.linkedin.com/company/${SOCIAL_LINKEDIN}`,
+  },
+  {
+    name: 'Discord',
+    path: `https://discord.gg/${SOCIAL_DISCORD}`,
+  },
+  {
+    name: 'GitHub',
+    path: `https://github.com/${SOCIAL_GITHUB}`,
+  },
+  {
+    name: 'Twitter',
+    path: `https://twitter.com/${SOCIAL_TWITTER}`,
+  },
+] as const
 
 export const IRON_SESSION_CONFIG = {
   cookieName: `siwe ${SITE_NAME}`,
