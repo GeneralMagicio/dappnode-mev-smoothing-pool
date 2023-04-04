554554ca8b2f301838bc9be1d8d0061a46189560
Vitor <marthendalnunes@gmail.com>
2023-04-04 20:56:11 -0300

    feat: add latest blocks table

---
+++ src/components/tables/LatestBlocksTable/components/Skeleton.tsx
@@ -0,0 +1,27 @@
+interface SkeletonProps {
+  title: string
+}
+
+export function Skeleton({ title }: SkeletonProps) {
+  return (
+    <div className="h-[650px] w-full overflow-hidden rounded-lg bg-white px-6">
+      <h3 className="p-6 text-2xl font-bold leading-8 text-DAppDeep">
+        {title}
+      </h3>
+      {[...Array(7)].map(() => (
+        <div
+          key={crypto.randomUUID()}
+          className="mb-8 flex w-full items-center justify-between">
+          <div>
+            <div className="h-5 w-[500px] animate-pulse rounded-sm bg-DAppLight" />
+            <div className="mt-3 h-4 w-[300px] animate-pulse rounded-sm bg-DAppLight" />
+          </div>
+          <div>
+            <div className="h-5 w-[110px] animate-pulse rounded-sm bg-DAppLight" />
+            <div className="mt-3 mr-0 ml-auto h-4 w-[60px] animate-pulse rounded-sm bg-DAppLight" />
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
