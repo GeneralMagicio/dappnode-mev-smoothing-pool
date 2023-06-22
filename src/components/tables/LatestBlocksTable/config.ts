6fc2470c0278c54694c01406c0d49f829d243692
Kilter <mateodaza@gmail.com>
2023-06-22 16:49:25 -0500

    Update config.ts

--- src/components/tables/LatestBlocksTable/config.ts
+++ src/components/tables/LatestBlocksTable/config.ts
@@ -2,7 +2,7 @@ export const PAGE_SIZE = 10
 
 export const headerTooltip = {
   slot: 'Slot of the block',
-  proposer: 'Address of the validator',
+  proposer: 'Key of the validator',
   rewardType: 'Type of the reward (vanila or mev)',
   reward: 'Reward of the block',
 }
